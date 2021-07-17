3194ad421fe0fa3bff4830a04b0a88237b0bb215
knrn16180 <25254561+knrn16180@users.noreply.github.com>
2021-07-17 14:34:53 +0300

    Finalize index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,36 @@
-// Логика старт скрина
-
 const startscreen = document.querySelector(".startscreen");
 const canvas = document.getElementById("game");
 const youlostscreen = document.querySelector(".youlostscreen");
-
-// function startGame(e) {
-//     if (e.key === 'Enter') {
-//         startscreen.style.display = "none";
-//         canvas.style.display = "block";
-//     };
-// }
-
-// document.addEventListener('keypress', startGame);
-
-//const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+let failSound;
+let eatSound;
+let bgmMusic;
+
 const ground = new Image();
-ground.src = "img/ground-test3.png";
+ground.src = "img/background.gif";
+
+const headImg = new Image();
+headImg.src = "img/head-front.gif";
 
 const foodImg = new Image();
 foodImg.src = "img/food.gif";
 
-const headImg = new Image();
-headImg.src = "img/head-front.gif";
+// const bananaImg = new Image();
+// bananaImg.src = "img/banana.gif";
+
+// const gumImg = new Image();
+// gumImg.src = "img/gum.gif";
+
+// const cherryImg = new Image();
+// cherryImg.src = "img/cherry.gif";
+
+// const peachImg = new Image();
+// peachImg.src = "img/peach.gif";
+
+// const foodArray = ["bananaImg", "gumImg", "cherryImg", "peachImg"];
+
+//let randomFoodImg = foodArray[Math.floor(Math.random() * foodArray.length)];
 
 let box = 22;
 
@@ -40,9 +47,42 @@ snake[0] = {
     y: 10 * box
 };
 
-document.addEventListener("keydown", changeDirection);
-
 let dir;
+
+class sound {
+    constructor(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "none");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function () {
+            this.sound.play();
+        };
+        this.stopSound = function () {
+            this.sound.pause();
+            this.sound.currentTime = 0;
+        };
+    }
+}
+
+function startGame(e) {
+    if (e.key === 'Enter') {
+        startscreen.style.display = "none";
+        canvas.style.display = "block";
+
+        bgmMusic = new sound("audio/bgm-music.mp3");
+        bgmMusic.volume = "0.1";
+        bgmMusic.play();
+        bgmMusic.sound.setAttribute("loop", "");
+        //bgmMusic.sound.setAttribute("volume", "0.1");
+
+        document.removeEventListener('keypress', startGame);
+    };
+}
+
+document.addEventListener('keypress', startGame);
 
 function changeDirection(event) {
     if (event.key === "ArrowLeft" && dir != "right")
@@ -55,6 +95,8 @@ function changeDirection(event) {
         dir = "down";
 }
 
+document.addEventListener("keydown", changeDirection);
+
 function generateRandomColor() {
     return "#" +
         (Math.floor(Math.random() * 0x1000000) + 0x1000000)
@@ -65,10 +107,8 @@ function generateRandomColor() {
 function eatTail(head, arr) {
     for (let i = 0; i < arr.length; i++) {
         if (head.x == arr[i].x && head.y == arr[i].y) {
-            console.log("GAME OVER");
-            clearInterval(game);
-            youlostscreen.style.display = "block";
-
+            restart();
+            //youlostscreen.style.display = "block";
         }
     }
 }
@@ -78,12 +118,16 @@ function drawGame() {
 
     ctx.drawImage(foodImg, food.x, food.y);
 
-    ctx.fillStyle = "white";
-    ctx.font = "20px Arial";
-    ctx.fillText("Score: " + score, box * 2.5, box * 1.7);
+    ctx.fillStyle = "black";
+    ctx.font = "14px Arial";
+    ctx.fillText("Score: " + score, box * 1, 40);
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
+
+    failSound = new sound("audio/fail-sound.mp3");
+    eatSound = new sound("audio/eat-sound.mp3");
+
 
     for (let i = 0; i < snake.length; i++) {
         // ctx.fillStyle = i == 0 ? "green" : "red";
@@ -91,15 +135,23 @@ function drawGame() {
             ctx.drawImage(headImg, snakeX, snakeY);
         }
         else if (i !== 0) {
+
+            drawBorder(snake[i].x, snake[i].y, box, box)
+
+            function drawBorder(xPos, yPos, width, height, thickness = 1) {
+                ctx.fillStyle = '#000000';
+                ctx.fillRect(xPos - (thickness), yPos - (thickness), width + (thickness * 2), height + (thickness * 2));
+            }
+            
             ctx.fillStyle = generateRandomColor();
             ctx.fillRect(snake[i].x, snake[i].y, box, box);
         }
     }
 
     if (snakeX == food.x && snakeY == food.y) {
+        eatSound.play();
         score++;
         food = {
-            // сюда добавить генерацию случайной еды? выбор еды из массива
             x: Math.floor((Math.random() * 17 + 1)) * box,
             y: Math.floor((Math.random() * 15 + 3)) * box,
         };
@@ -108,9 +160,6 @@ function drawGame() {
 
     if (snakeX < box || snakeX > box * 17
         || snakeY < 3 * box || snakeY > box * 17) {
-        console.log("GAME OVER");
-
-        //clearInterval(game);
         //youlostscreen.style.display = "block";
         restart()
     }
@@ -131,7 +180,12 @@ function drawGame() {
 }
 
 function restart() {
+    failSound.play();
+
+    //console.log("function has been executed");
+
     score = 0;
+    dir = "";
 
     snake = [];
     snake[0] = {
@@ -139,9 +193,12 @@ function restart() {
         y: 10 * box
     };
 
-    dir = "";
+    food = {
+        x: Math.floor((Math.random() * 17 + 1)) * box,
+        y: Math.floor((Math.random() * 15 + 3)) * box,
+    };
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.context.width, canvas.context.height);
     clearInterval(game);
 
 // тут наверное можно показвыать you lost экран, потом по enter вызывать drawGame()
